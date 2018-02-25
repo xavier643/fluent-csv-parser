394e062b55fcb17d770594e36fd3293f91380d5d
xavier643 <36451174+xavier643@users.noreply.github.com>
2018-02-24 19:49:48 -0500

    Add files via upload

--- src/Parser.php
+++ src/Parser.php
@@ -4,38 +4,61 @@ namespace CSVParser;
 
 class Parser {
 
-    /*
-     * @var $path the file path to the csv file to be parsed.
-     * @var $headers The column headers for the CSV file that give context to the column values.
-     * @var $delimiter the delimiter value that separates column values for this CSV file.
+    /**
+     * @var path the file path to the csv file to be parsed.
      */
-
     private $path;
-    private $headers = [];
+
+    /**
+     * @var headers The column headers for the CSV file that give context to the column values.
+     */
+    private $headers;
+
+    /**
+     * @var delimiter the delimiter value that separates column values for this CSV file.
+     */
     private $delimiter;
 
-    function __construct ($path, $headers = [], $delimiter = ',') {
-
+    function __construct ($path, $headers = [], $delimiter = ',') 
+    {
         /*
          * Check that the path does exist or throws an exception
          * Checks that the delimiter is a , | or /t
          * Fills headers, if it's empty grabs first line from CSV instead
          */
 
-        if(!file_exists($path)) throw new Exception("{$path} is not a valid file.");
+        if(!file_exists($path)) 
+            throw new \Exception("{$path} is not a valid file.");
         $this->path = $path; //fill path
 
-        if($delimiter == ',' || $delimiter == '|' || $delimiter == '\t') { //verify delimiter
-            $this->delimiter = $delimiter; //fill delimiter
-        }
+        if(!in_array($delimiter, [',', '|', '\t']))  //verify delimiter
+            throw new Exception("A delimiter of {$delimiter} is not supported.");    
+        $this->delimiter = $delimiter; //fill delimiter
 
         $this->headers = $headers;
-        if(empty($headers)) {
-            /* 
-             * Pull first row of data from the file to fill the headers since was empty
+        if(empty($headers)) 
+        {
+            /**
+             * @var path_open will open up path for reading
              */
+            $path_open = fopen($path, 'r');
+            /**
+             * @var data is an array of data from the csv file
+             */
+            $data = fgetcsv($path_open, 1000, $this->delimiter);
+
+            //not using str_getcsv() because it makes array of arrays from whole CSV
+
 
             //$this->headers = first row of CSV
+            //line 31 will need to set path to an open resource in order to read this->path = fopen($path);
+            //will make path = to the resource.  File pointer.  function of fgetcsv in order to read in the rows.
+            //close the path when done
         }
+    }
+
+    public function headers()
+    {
+        return $this->headers;
     }
 }
--- test/ParserTest.php
+++ test/ParserTest.php
@@ -6,32 +6,60 @@ use CSVParser\Parser;
 
 final class ParserTests extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var $file is for a test file to verify object constructed
+     */
     protected $file;
 
     public function setUp()
     {
-        $this->file = tmpfile();
+        /*
+         * create a temporary file and place it into $file
+         */
+        $this->file = tempnam(sys_get_temp_dir(), 'csv_parser_test'); 
     }
 
     public function tearDown()
     {
-        fclose($this->file);
+        /*
+         * removes the temporary file
+         */
+        unlink($this->file);
     }
 
-    /*
+    /**
      * @test
      */
     public function testConstruction()
     {
+        /*
+         * $subject is the object created from parser.php and filled with the temp file
+         */
         $subject = new Parser($this->file);
 
+        /*
+         * verfies the object and test file are created
+         */
         $this->assertInstanceOf('CSVParser\Parser', $subject);
     }
 
-    /*
+    /**
      * @test
      */
     public function testConstructionWithHeader()
     {
+        $subject = new Parser($this->file, ['this, that, them']);
+
+        $this->assertInstanceOf('CSVParser\Parser', $subject);
+        $this->assertEquals(['this, that, them'], $subject->headers());
+    }
+
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage file_does_not_exist is not a valid file.
+     */
+    public function testConstructionNoFile() {
+        $subject = new Parser('file_does_not_exist');
     }
 }
