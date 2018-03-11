6f79ad740db1b437ca6fe97b6088227492edbc66
xavier643 <36451174+xavier643@users.noreply.github.com>
2018-03-10 22:32:49 -0500

    Update ParserTest.php

--- test/ParserTest.php
+++ test/ParserTest.php
@@ -76,9 +76,9 @@ final class ParserTests extends \PHPUnit_Framework_TestCase
         fwrite($file_open, "social,");
         fwrite($file_open, "123");
         fclose($file_open);
-        $test_array = array('name', 'social', 123);
+        $expected = array('name', 'social', 123);
 
         $subject = new Parser($this->file, ',');
-        $this->assertEquals($test_array, $subject->headers());
+        $this->assertEquals($expected, $subject->headers());
     }
 }
