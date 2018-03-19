2d075ec853d0e9148ddfa5da2ae3f83805accda8
d3r3kdrumm0nd <derek.ac.drummond@gmail.com>
2018-03-18 20:31:49 -0500

    Refactors Parser and unit test to acheive 100% code coverage.

--- src/Parser.php
+++ src/Parser.php
@@ -21,29 +21,25 @@ class Parser {
 
     function __construct ($path, $delimiter = ',', $headers = []) 
     {
-        /*
+        /**
          * Check that the path does exist or throws an exception
          * Checks that the delimiter is a , | or /t
          * Fills headers, if it's empty grabs first line from CSV instead
          */
-
         if(!file_exists($path)) 
             throw new \Exception("{$path} is not a valid file.");
-        $this->path = fopen($path, 'r'); //fill path
+        $this->path = fopen($path, 'r'); //Open the CSV file as a readonly file.
 
-        if(!in_array($delimiter, [',', '|', '\t']))  //verify delimiter
-            throw new Exception("A delimiter of {$delimiter} is not supported.");    
+
+        if(!in_array($delimiter, [',', '|', '\t'])) //verify delimiter
+            throw new \Exception("A delimiter of {$delimiter} is not supported.");    
         $this->delimiter = $delimiter; //fill delimiter
 
-        
+        //If the $headers parameter is empty read the first row from the CSV file which is assumed to be the column headers
         if(empty($headers)) 
-        {
-            //following line currently gets whole csv? Not just first line?
-            $data = fgetcsv($this->path, 0, $this->delimiter);
-            $this->headers = $data;
-        } else {
+            $this->headers = fgetcsv($this->path, 0, $this->delimiter);
+        else
             $this->headers = $headers;
-        }
     }
 
     public function getHeaders()
@@ -51,3 +47,4 @@ class Parser {
         return $this->headers;
     }
 }
+
